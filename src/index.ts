6a45580992a404eeae5f6a81fe4570d937ea4096
Owen Bellish <shadowguy224@live.com>
2019-09-12 13:13:14 -0400

    docs: copy from original type class

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,47 @@
 // @ts-ignore
 const { getPromiseDetails } = process.binding('util');
 
+/**
+ * The class for deep checking Types
+ */
 export default class Type {
-
+	/**
+	 * The value to generate a deep Type of
+	 * @type {*}
+	 */
 	public value: unknown;
 
+	/**
+	 * The shallow type of this
+	 * @type {string}
+	 */
 	public is: string;
 
+	/**
+	 * The parent of this Type
+	 * @type {?Type}
+	 * @private
+	 */
 	private parent: Type | null;
 
+	/**
+	 * The child keys of this Type
+	 * @type {Map<string, Type>}
+	 * @private
+	 */
 	private childKeys: Map<string, Type>;
 
+	/**
+	 * The child values of this Type
+	 * @type {Map<string, Type>}
+	 * @private
+	 */
 	private childValues: Map<string, Type>;
 
+	/**
+	 * @param {*} value The value to generate a deep Type of
+	 * @param {Type} [parent=null] The parent value used in recursion
+	 */
 	public constructor(value: unknown, parent: Type | null = null) {
 		this.value = value;
 
@@ -26,16 +55,31 @@ export default class Type {
 		this.childValues = new Map();
 	}
 
+	/**
+	 * The type string for the children of this Type
+	 * @type {string}
+	 * @readonly
+	 * @private
+	 */
 	private get childTypes(): string {
 		if (!this.childValues.size) return '';
 		return `<${(this.childKeys.size ? `${Type.list(this.childKeys)}, ` : '') + Type.list(this.childValues)}>`;
 	}
 
+	/**
+	 * The full type string generated.
+	 * @returns {string}
+	 */
 	public toString(): string {
 		this.check();
 		return this.is + this.childTypes;
 	}
 
+	/**
+	 * Walks the linked list backwards, for checking circulars.
+	 * @yields {?Type}
+	 * @private
+	 */
 	private *parents(): Generator<Type, void, unknown> {
 		// eslint-disable-next-line @typescript-eslint/no-this-alias, no-cond-assign, consistent-this
 		let current: Type | null = this;
@@ -43,22 +87,41 @@ export default class Type {
 		while (current = current.parent) yield current;
 	}
 
+	/**
+	 * Checks if the value of this Type is a circular reference to any parent.
+	 * @returns {boolean}
+	 * @private
+	 */
 	private isCircular(): boolean {
 		for (const parent of this.parents()) if (parent.value === this.value) return true;
 		return false;
 	}
 
+	/**
+	 * The subtype to create based on this.value's sub value.
+	 * @param {*} value The sub value
+	 * @private
+	 */
 	private addValue(value: unknown): void {
 		const child = new Type(value, this);
 		this.childValues.set(child.is, child);
 	}
 
+	/**
+	 * The subtype to create based on this.value's entries.
+	 * @param {Array<string, *>} entry The entry
+	 * @private
+	 */
 	private addEntry([key, value]: [string, unknown]): void {
 		const child = new Type(key, this);
 		this.childKeys.set(child.is, child);
 		this.addValue(value);
 	}
 
+	/**
+	 * Get the deep type name that defines the input.
+	 * @private
+	 */
 	private check(): void {
 		if (Object.isFrozen(this)) return;
 		const promise = getPromiseDetails(this.value);
@@ -70,6 +133,11 @@ export default class Type {
 		Object.freeze(this);
 	}
 
+	/**
+	 * Resolves the type name that defines the input.
+	 * @param {*} value The value to get the type name of
+	 * @returns {string}
+	 */
 	public static resolve(value: any): string {
 		const type = typeof value;
 		switch (type) {
@@ -80,6 +148,12 @@ export default class Type {
 		}
 	}
 
+	/**
+	 * Joins the list of child types.
+	 * @param {Map<string, Type>} values The values to list
+	 * @returns {string}
+	 * @private
+	 */
 	private static list(values: Map<string, Type>): string {
 		return values.has('any') ? 'any' : [...values.values()].sort().join(' | ');
 	}
