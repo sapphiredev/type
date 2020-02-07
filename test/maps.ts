066dc2d8b5e3299aa86b27b7b0d5098134689118
bdistin <bdistin@gmail.com>
2020-02-07 16:26:14 -0600

    more coverage

--- test/maps.ts
+++ test/maps.ts
@@ -12,3 +12,7 @@ ava('map(same-type)', (test): void => {
 ava('map(different-type)', (test): void => {
 	test.is(new Type(new Map<string, string | number>([['text', 'abc'], ['digit', 1]])).toString(), 'Map<string, number | string>');
 });
+
+ava('map(mixed with object)', (test): void => {
+	test.is(new Type(new Map<string, any>([['text', 'abc'], ['digit', 1], ['object', {}]])).toString(), 'Map<string, any>');
+});
--- test/objects.ts
+++ test/objects.ts
@@ -5,6 +5,10 @@ ava('object(generic)', (test): void => {
 	test.is(new Type({}).toString(), 'any');
 });
 
+ava('object(null)', (test): void => {
+	test.is(new Type(null).toString(), 'null');
+});
+
 ava('object(custom)', (test): void => {
 	class Foo {}
 	test.is(new Type(new Foo()).toString(), 'Foo');
