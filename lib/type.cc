5d8533815e7abfb2a5c9c048f64a05df4fc5b8db
Vlad Frangu <kingdgrizzle@gmail.com>
2021-04-28 20:17:12 +0300

    chore: styling because I'm a bitch 💅

--- lib/type.cc
+++ lib/type.cc
@@ -1,26 +1,30 @@
 #include <nan.h>
 
 void GetPromiseDetails(const Nan::FunctionCallbackInfo<v8::Value>& args) {
-    if (!args[0]->IsPromise())
+    if (!args[0]->IsPromise()) {
         return;
-
+	}
 
     auto isolate = args.GetIsolate();
 
     v8::Local<v8::Promise> promise = args[0].As<v8::Promise>();
-
     int state = promise->State();
+
     v8::Local<v8::Value> values[2] = { v8::Integer::New(isolate, state) };
     size_t number_of_values = 1;
-    if (state != v8::Promise::PromiseState::kPending)
+
+    if (state != v8::Promise::PromiseState::kPending) {
         values[number_of_values++] = promise->Result();
+	}
+
     v8::Local<v8::Array> ret = v8::Array::New(isolate, values, number_of_values);
     args.GetReturnValue().Set(ret);
 }
 
 void GetProxyDetails(const Nan::FunctionCallbackInfo<v8::Value>& args) {
-	if (!args[0]->IsProxy())
+	if (!args[0]->IsProxy()) {
 		return;
+	}
 
 	v8::Local<v8::Proxy> proxy = args[0].As<v8::Proxy>();
 
