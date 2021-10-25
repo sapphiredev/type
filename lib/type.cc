d249d38d84885fab828197b17c405f04bc88a6ef
Jeroen Claassens <support@favware.tech>
2021-10-25 20:20:42 +0200

    chore: use consistent indents in c code

--- lib/type.cc
+++ lib/type.cc
@@ -1,24 +1,24 @@
 #include <nan.h>
 
 void GetPromiseDetails(const Nan::FunctionCallbackInfo<v8::Value>& args) {
-    if (!args[0]->IsPromise()) {
-        return;
+	if (!args[0]->IsPromise()) {
+		return;
 	}
 
-    auto isolate = args.GetIsolate();
+	auto isolate = args.GetIsolate();
 
-    v8::Local<v8::Promise> promise = args[0].As<v8::Promise>();
-    int state = promise->State();
+	v8::Local<v8::Promise> promise = args[0].As<v8::Promise>();
+	int state = promise->State();
 
-    v8::Local<v8::Value> values[2] = { v8::Integer::New(isolate, state) };
-    size_t number_of_values = 1;
+	v8::Local<v8::Value> values[2] = { v8::Integer::New(isolate, state) };
+	size_t number_of_values = 1;
 
-    if (state != v8::Promise::PromiseState::kPending) {
-        values[number_of_values++] = promise->Result();
+	if (state != v8::Promise::PromiseState::kPending) {
+		values[number_of_values++] = promise->Result();
 	}
 
-    v8::Local<v8::Array> ret = v8::Array::New(isolate, values, number_of_values);
-    args.GetReturnValue().Set(ret);
+	v8::Local<v8::Array> ret = v8::Array::New(isolate, values, number_of_values);
+	args.GetReturnValue().Set(ret);
 }
 
 void GetProxyDetails(const Nan::FunctionCallbackInfo<v8::Value>& args) {
@@ -45,9 +45,9 @@ void GetProxyDetails(const Nan::FunctionCallbackInfo<v8::Value>& args) {
 }
 
 void Init(v8::Local<v8::Object> exports) {
-    v8::Local<v8::Context> context = exports->CreationContext();
+	v8::Local<v8::Context> context = exports->CreationContext();
 
-    exports->Set(context, Nan::New("getPromiseDetails").ToLocalChecked(), Nan::New<v8::FunctionTemplate>(GetPromiseDetails)->GetFunction(context).ToLocalChecked());
+	exports->Set(context, Nan::New("getPromiseDetails").ToLocalChecked(), Nan::New<v8::FunctionTemplate>(GetPromiseDetails)->GetFunction(context).ToLocalChecked());
 	exports->Set(context, Nan::New("getProxyDetails").ToLocalChecked(), Nan::New<v8::FunctionTemplate>(GetProxyDetails)->GetFunction(context).ToLocalChecked());
 }
 
