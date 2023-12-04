3b42e837c50d760190f95b2c9e788d424176ee89
Jeroen Claassens <support@favware.tech>
2023-12-04 21:32:23 +0100

    fix: properly split CJS and ESM

---
+++ tsup.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig, type Options } from 'tsup';
+
+const baseOptions: Options = {
+	clean: true,
+	entry: ['src/index.ts'],
+	dts: true,
+	minify: false,
+	skipNodeModulesBundle: true,
+	sourcemap: true,
+	target: 'es2021',
+	tsconfig: 'src/tsconfig.json',
+	keepNames: true,
+	treeshake: true
+};
+
+export default [
+	defineConfig({
+		...baseOptions,
+		outDir: 'dist/cjs',
+		format: 'cjs',
+		outExtension: () => ({ js: '.cjs' })
+	}),
+	defineConfig({
+		...baseOptions,
+		outDir: 'dist/esm',
+		format: 'esm'
+	})
+];
