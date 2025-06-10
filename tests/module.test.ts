394763a4f57fbb1470976b7fa21baa58bfa85084
Jeroen Claassens <support@favware.tech>
2025-06-10 22:49:26 +0200

    chore: merge pull my finger request

--- tests/module.test.ts
+++ tests/module.test.ts
@@ -4,7 +4,7 @@ import { Type } from '../src/lib/index.js';
 describe('Modules', () => {
 	test('discord.js', () => {
 		expect(new Type(discordjs).toString()).toMatchInlineSnapshot(
-			`"Proxy<Record<string, Array<string> | AsyncFunction(1-arity) | AsyncFunction(7-arity) | Function | Function(0-arity) | Function(1-arity) | Function(2-arity) | Function(3-arity) | Function(4-arity) | Function(5-arity) | Function(6-arity) | GeneratorFunction(1-arity) | Record<string, Array<string> | AsyncFunction(1-arity) | AsyncFunction(7-arity) | Function | Function(0-arity) | Function(1-arity) | Function(2-arity) | Function(3-arity) | Function(4-arity) | Function(5-arity) | Function(6-arity) | GeneratorFunction(1-arity) | Record<string, number | string> | Set<string> | Snowflake | string> | Set<string> | Snowflake | string | void>>"`
+			`"Proxy<Record<string, Array<string> | AsyncFunction(1-arity) | AsyncFunction(7-arity) | Function | Function(0-arity) | Function(1-arity) | Function(2-arity) | Function(3-arity) | Function(4-arity) | Function(5-arity) | Function(6-arity) | GeneratorFunction(1-arity) | Record<string, Function(1-arity)> | Set<string> | Snowflake | string | void>>"`
 		);
 	});
 });
